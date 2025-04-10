775948912e9ffdbf0ba6f7d86548fc0262af016b
DymuX17 <patrykd1707@gmail.com>
2025-04-10 13:12:27 +0200

    adding BookController to project

---
+++ src/main/java/com/example/librarymanagement/controller/BookController.java
@@ -0,0 +1,33 @@
+package com.example.librarymanagement.controller;
+
+import com.example.librarymanagement.model.Book;
+import com.example.librarymanagement.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+    }
+
+    @PutMapping("/{id}/borrow")
+    public Book borrowBook(@PathVariable Long id) {
+        return bookService.borrowBook(id);
+    }
+
+}
